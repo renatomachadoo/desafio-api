23dc944b424ca48ba7b7281d2222e8ca451977cb
Renato Machado <81891866+Machadoo7@users.noreply.github.com>
2024-08-31 00:04:14 +0100

    atualização migrations

--- src/database/knex/migrations/20240830225717_movies_tags.js
+++ src/database/knex/migrations/20240830225717_movies_tags.js
@@ -1,7 +1,7 @@
 exports.up = knex => knex.schema.createTable("movies_tags", table => {
   table.increments("id")
-  table.integer("note_id").references("id").inTable("movies_notes")
-  table.integer("user_id").references("id").inTable("users").onDelete("CASCADE")
+  table.integer("note_id").references("id").inTable("movies_notes").onDelete("CASCADE")
+  table.integer("user_id").references("id").inTable("users")
   table.text("name")
   table.timestamp("created_at").default(knex.fn.now())
   table.timestamp("updated_at").default(knex.fn.now())
