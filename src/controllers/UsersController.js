9fe2d481be28129f0d2854f078280bd2860c2bd9
Renato Machado <81891866+Machadoo7@users.noreply.github.com>
2024-08-31 00:21:58 +0100

    criação de utilizadores

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,28 @@
+const { hash, compare } = require("bcryptjs")
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+
+class UsersController{
+    async create(request, response){
+        const {name, email, password, avatar} = request.body
+
+        const [checkUserExists] = await knex("users").where({ email })
+
+        if(checkUserExists){
+            throw new AppError("Este e-mail já está em uso.")
+        }
+
+        const hashedPassword = await hash(password, 8)
+
+        await knex("users").insert({
+            name,
+            email,
+            password : hashedPassword,
+            avatar
+        })
+
+        return response.status(201).json()
+    }
+}
+
+module.exports = UsersController
