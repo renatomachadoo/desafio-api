bfc9ebc164998eb2bbebfd60bf9d584b1f823cd6
Renato Machado <renatomachado176@gmail.com>
2024-09-28 12:08:31 +0100

    Update User Route

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs")
+const { hash, compare } = require("bcryptjs")
 const knex = require("../database/knex")
 const AppError = require("../utils/AppError")
 
@@ -22,6 +22,44 @@ class UsersController{
         })
 
         return response.status(201).json()
+    }
+
+    async update(request, response){
+        const { name, email, old_password, new_password} = request.body
+        const user_id = request.user.id
+
+        const user = await knex("users").where({ id : user_id }).first()
+
+        if(!user){
+            throw new AppError("Utilizador não encontrado.", 401)
+        }
+
+        const userWithEmail = await knex("users").where({ email }).first()
+
+        if(userWithEmail && userWithEmail.id !== user.id){
+            throw new AppError("Este e-mail já está a ser utilizado.", 401)
+        }
+
+        user.name = name ?? user.name
+        user.email = email ?? user.email
+
+        if(new_password && !old_password){
+            throw new AppError("É necessário confirmar a palavra-passe antiga para alterar a palavra-passe.", 401)
+        }
+
+        if(new_password && old_password){
+            const passwordMatched = await compare(old_password, user.password)
+
+            if(!passwordMatched){
+                throw new AppError("As palavras-passe não coincidem.")
+            }
+
+            user.password = await hash(new_password, 8) 
+        }
+
+        await knex("users").update(user).where({ id : user_id })
+
+        return response.json({ user })
     }
 }
 
