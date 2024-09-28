0af2f2082232fc468b885d2c974ada4d8d2d2001
Renato Machado <renatomachado176@gmail.com>
2024-09-28 11:35:25 +0100

    Session Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require("express")
 
 const usersRouter = require("./users.routes")
 const moviesNotesRouter = require("./moviesNotes.routes")
+const sessionsRouter = require("./sessions.routes")
 
 const routes = Router()
 
 routes.use("/users", usersRouter)
 routes.use("/movies_notes", moviesNotesRouter)
+routes.use("/sessions", sessionsRouter)
 
 module.exports = routes
